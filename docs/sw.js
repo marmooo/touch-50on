297aadfc7cec68aa052bbcc74dad8af4813ac21b
marmooo <marmooo@users.noreply.github.com>
2020-12-12 11:09:32 +0900

    test

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2020-12-12 11:00';
+var CACHE_NAME = '2020-12-12 11:10';
 var urlsToCache = [
   '/touch-50on/',
   '/touch-50on/index.js',
--- src/drill.js
+++ src/drill.js
@@ -309,10 +309,10 @@ let audioUnlocked = false;
 function setScoringButton(problemBox, tegakiPanel, tehonPanel, objects, tegakiPads, word) {
   var scoring = problemBox.shadowRoot.querySelector('#scoring');
   scoring.addEventListener('click', function() {
-    if (!audioUnlocked) {
-      unlockAudio();
-      audioUnlocked = true;
-    }
+    // if (!audioUnlocked) {
+    //   unlockAudio();
+    //   audioUnlocked = true;
+    // }
     getProblemScores(tegakiPanel, tehonPanel, objects, tegakiPads).then(scores => {
       if (scores.every(score => score >= 80)) {
         problemBox.shadowRoot.querySelector('#guard').style.height = '100%';
