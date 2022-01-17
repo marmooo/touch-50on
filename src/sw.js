ed16080c40d61643c767a4ec9c9042dbda7a6e3f
marmooo <marmooo@users.noreply.github.com>
2022-01-17 09:26:20 +0900

    bump signature_pad from 3.0.0-beta.4 to 4.0.1

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-28 23:15';
+var CACHE_NAME = '2022-01-17 09:25';
 var urlsToCache = [
   "/touch-50on/",
   "/touch-50on/index.js",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/touch-50on/mp3/incorrect1.mp3",
   "/touch-50on/mp3/stupid5.mp3",
   "/touch-50on/favicon/original.svg",
-  "/touch-50on/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.1/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
 ];
 
