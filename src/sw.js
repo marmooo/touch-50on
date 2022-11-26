d7272f9569af4b642b8ff13b8375666bc52d45a0
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:00:45 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-11-15 10:10";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/touch-50on/",
   "/touch-50on/index.js",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/touch-50on/mp3/incorrect1.mp3",
   "/touch-50on/mp3/stupid5.mp3",
   "/touch-50on/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.1/dist/signature_pad.umd.min.js",
 ];
 
