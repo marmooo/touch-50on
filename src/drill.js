644a4d56d3835f30010b0ffb2e1b79909d250ab8
marmooo <marmooo@users.noreply.github.com>
2023-05-10 00:25:12 +0900

    cleanup

--- src/drill.js
+++ src/drill.js
@@ -867,9 +867,3 @@ document.addEventListener("click", unlockAudio, {
   once: true,
   useCapture: true,
 });
-
-// disable troublesome iOS features
-// - double tap zoom
-document.ondblclick = (e) => {
-  e.preventDefault();
-};
