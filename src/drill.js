a99871d794b91dcc9744d60351c92c528fed9522
marmooo <marmooo@users.noreply.github.com>
2024-06-16 09:38:00 +0900

    bump signature_pad from 5.0.1 to 5.0.2

--- src/drill.js
+++ src/drill.js
@@ -1,4 +1,4 @@
-import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.0.1/+esm";
+import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.0.2/+esm";
 
 const audioContext = new globalThis.AudioContext();
 const audioBufferCache = {};
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-05-20 01:29";
+const CACHE_NAME = "2024-06-16 09:37";
 const urlsToCache = [
   "/touch-50on/",
   "/touch-50on/index.js",
